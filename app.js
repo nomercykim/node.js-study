f5a07e3e76e7adde6268ffd6d0e50b260aeb1727
nomercykim <id6word@gmail.com>
2023-08-03 23:46:16 +0900

    node.js 공부 0803

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
+// const express = require('express');
+// const helmet = require("helmet");
+// const ejs = require("ejs");
+// const app = express();
+
+
+// app.set('view engin', 'ejs');
+// app.set('views', './views');
+// app.use('/public', express.static(__dirname+'/public'));
+
+
+// app.use(helmet());
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+// const mainRouter = require('./router/mainRouter.js');
+// app.use('/', mainRouter);
+
+// app.listen(3000,function(req,res){
+//     console.log("Example app listening on Port 3000.")
+// }) 
+
+
+
+
+//express 도구를 불러오는 부분
+//도구를 불러와 변수에 담아 사용합니다.
+
 const express = require('express');
 const helmet = require("helmet");
-// import express from "express";
-// import helmet from "helmet";
-const app = express();
 const ejs = require("ejs");
 
+//결국 express도 많은 기능을 담고 있는 자바스크립트 함수로 볼 수 있으므로
+//express 함수가 반환하는 서버 기능들을 다시 app에 담아 사용합니다.
 
-app.set('view engin', ejs);
-app.set('view', './view');
-app.use('/public', express.static(__dirname+'/public'));
+const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded());
 
-const mainRouter = require('./router/mainRouter');
-app.use('/', mainRouter);
+//분리한 라우터(주소)들을 한 곳에 가져옵니다.
 
-app.listen(3000,function(req,res){
-    console.log("Example app listening on Port 3000.")
-}) 
+const mainRouter = require('./router/mainRouter.js')
+app.use('/', mainRouter)
+
+
+
+//서버를 켜는 코드입니다.
+//3000은 컴퓨터의 몇 번방을 사용할 것인지, 즉 포트에 대한 설정을 하는 부분입니다.
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,26 +1,49 @@
-const express = require("express");
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 
 
-router.get("/",function(req,res){
+// router.get("/",function(req,res){
+//     res.render('index', {titile:"EJS 메인페이지"});
 
-    let page = req.query.page;
-    console.log("[QUERY]");
-    console.log(page);
+// })
 
-    res.send({"key":"value"});
+// router.get("/about", function(req,res){
+//     res.send('About page');
+// })
+
+
+// router.post("/postapi",function(req,res){
+//     let body = req.body;
+//     console.log(body);
+//     res.send("POST API");
+
+// })
+
+
+// module.exports = router;
+
+
+
+const express = require('express')
+const router = express.Router()
+
+
+
+// Home page route
+router.get('/', function (req, res) {
+ res.render('index.ejs',{"title":"EJS 메인페이지"})
 })
 
-router.post("/postapi",function(req,res){
-    let body = req.body;
-    console.log(body);
-    res.send("POST API");
 
+router.post('/', function (req, res) {
+ res.send({"key":"value"})
 })
 
-router.get("/about", function(req,res){
-    res.send('About page');
+
+// About page route
+router.get('/about', function (req, res) {
+ res.send('어바웃 페이지')
 })
 
 module.exports = router;
